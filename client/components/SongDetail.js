c3c41dc68e8383a021aaf7a0eb287f8f2493855f
RODRIGO SARMENTO XAVIER <rodrigosarmentoxx@gmail.com>
2021-04-09 12:06:01 -0300

    a

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import fetchSong from "../queries/fetchSong";
+import { Link } from "react-router";
+import LyricCreate from "./LyricCreate";
 
 class SongDetail extends Component {
   render() {
+    const song = this.props.data.song;
+
+    if (!song) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        <h3>Song Detail</h3>
+        <Link to={"/"}>Back</Link>
+        <h3>{song.title}</h3>
+        <LyricCreate songId={this.props.params.id} />
       </div>
     );
   }
 }
 
-export default SongDetail;
+export default graphql(fetchSong, {
+  options: (props) => {
+    return { variables: { id: props.params.id } };
+  },
+})(SongDetail);
